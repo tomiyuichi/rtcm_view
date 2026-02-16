30579e3b0e21288bd69aaed7fb477d39a6f09447
tomi <tomi@x1carbon7th.local>
2026-02-17 02:01:53 +0900

    added egui.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use eframe::egui;
+
 fn main() {
-    println!("Hello, world!");
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "egui test",
+        options,
+        Box::new(|_cc| Box::new(MyApp::default())),
+    );
+}
+
+#[derive(Default)]
+struct MyApp;
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello egui");
+            if ui.button("Click").clicked() {
+                println!("clicked");
+            }
+        });
+    }
 }
